8b29e83737969d1be1860c089fee2edbd7009045
大畔祐輝 <ooguro@ooguro.local>
2017-04-02 20:26:23 +0900

    spring-boot-initializr

---
+++ src/main/java/com/shuffle/team/ShuffleTeamApplication.java
@@ -0,0 +1,12 @@
+package com.shuffle.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuffleTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuffleTeamApplication.class, args);
+	}
+}
